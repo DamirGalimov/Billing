4b0782f58581969c304dc1f515917dac04603274
Artur <akun971@gmail.com>
2017-04-20 01:49:21 +0700

    Контролы добавились

--- Billing-View/EmployeeControl.Designer.cs
+++ Billing-View/EmployeeControl.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.hourlyPayEmployeeControl1 = new Billing_View.HourlyPayEmployeeControl();
+            this.salaryPayEmployeeControl1 = new Billing_View.SalaryPayEmployeeControl();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -42,15 +43,23 @@
             // 
             // hourlyPayEmployeeControl1
             // 
-            this.hourlyPayEmployeeControl1.Location = new System.Drawing.Point(3, 54);
+            this.hourlyPayEmployeeControl1.Location = new System.Drawing.Point(13, 48);
             this.hourlyPayEmployeeControl1.Name = "hourlyPayEmployeeControl1";
             this.hourlyPayEmployeeControl1.Size = new System.Drawing.Size(255, 141);
             this.hourlyPayEmployeeControl1.TabIndex = 1;
+            // 
+            // salaryPayEmployeeControl1
+            // 
+            this.salaryPayEmployeeControl1.Location = new System.Drawing.Point(9, 48);
+            this.salaryPayEmployeeControl1.Name = "salaryPayEmployeeControl1";
+            this.salaryPayEmployeeControl1.Size = new System.Drawing.Size(262, 166);
+            this.salaryPayEmployeeControl1.TabIndex = 2;
             // 
             // EmployeeControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.salaryPayEmployeeControl1);
             this.Controls.Add(this.hourlyPayEmployeeControl1);
             this.Controls.Add(this.comboBox1);
             this.Name = "EmployeeControl";
@@ -63,5 +72,6 @@
 
         private System.Windows.Forms.ComboBox comboBox1;
         private HourlyPayEmployeeControl hourlyPayEmployeeControl1;
+        private SalaryPayEmployeeControl salaryPayEmployeeControl1;
     }
 }
